0e2906fbcf0b6d93dd116cd557a9ba6188c5f57c
Monir Hossain <51879416+mhsharkar2011@users.noreply.github.com>
2021-09-28 22:59:45 +0600

    Version_01
    28_09_2021

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+// // Navbar Fixed on Top
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-105px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+
+$(document).ready(function() {
+    // grab the initial top offset of the navigation 
+       var stickyNavTop = $('#navbar').offset().top;
+       
+       // our function that decides weather the navigation bar should have "fixed" css position or not.
+       var stickyNav = function(){
+        var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+             
+        // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+        // otherwise change it back to relative
+        if (scrollTop > stickyNavTop) { 
+            $('#navbar').addClass('sticky');
+        } else {
+            $('#navbar').removeClass('sticky'); 
+        }
+    };
+  
+    stickyNav();
+    // and run it again every time you scroll
+    $(window).scroll(function() {
+      stickyNav();
+    });
+  });
+
